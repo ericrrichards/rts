0541977b8584d3aa11edd1d95f1d965282cd6389
Eric Richards <ericrrichards@gmail.com>
2013-06-10 17:14:56 -0400

    restarting the core framework stuff

--- Core/App.cs
+++ Core/App.cs
@@ -12,184 +12,34 @@ namespace Core {
     using SlimDX;
     using SlimDX.Direct3D9;
 
-    public abstract class App {
-        public static App GApp;
-        public static Device GDevice;
-        public static Random Rand;
-
-        protected string MainWindowCaption;
-        protected DeviceType DevType;
-        protected CreateFlags RequestedVertexProcessing;
-        protected Direct3D D3DObject;
-        protected bool Paused;
-        protected PresentParameters PP;
-        private bool _running = true;
-
-        public IntPtr AppInstance { get; protected set; }
-        public Form MainWindow { get; protected set; }
-
-        protected App(IntPtr hinstance, string caption, DeviceType devType, CreateFlags requestedVP) {
-            MainWindowCaption = caption;
-            DevType = devType;
-            RequestedVertexProcessing = requestedVP;
-
-            AppInstance = hinstance;
-            MainWindow = null;
-            D3DObject = null;
-            Paused = false;
-            PP = new PresentParameters();
-            Rand = new Random();
-            InitMainWindow();
-            InitDirect3D();
-        }
-
-         ~App() {
-            if (D3DObject != null) {
-                D3DObject.Dispose();
-                D3DObject = null;
-            }
-            if (GDevice != null) {
-                GDevice.Dispose();
-                GDevice = null;
-            }
-        }
-
-        public virtual bool CheckDeviceCaps() {return true;}
-        public abstract void OnLostDevice();
-        public abstract void OnResetDevice();
-        public abstract void UpdateScene(float dt);
-        public abstract void DrawScene();
-
-        public virtual void InitMainWindow() {
-            MainWindow = new Form() {
-                Text = MainWindowCaption,
-                Width = 800,
-                Height = 600,
-                FormBorderStyle = FormBorderStyle.FixedSingle,
-                SizeGripStyle = SizeGripStyle.Hide,
-                StartPosition = FormStartPosition.CenterScreen,
-
-            };
-            MainWindow.Show();
-            MainWindow.Update();
-        }
-        public virtual void InitDirect3D() {
-            D3DObject = new Direct3D();
-            if (D3DObject == null) {
-                MessageBox.Show("Failed to create Direct3D object");
-                Exit();
-            }
-
-            var mode = D3DObject.GetAdapterDisplayMode(0);
-            if (!D3DObject.CheckDeviceType(0, DevType, mode.Format, mode.Format, true)) {
-                MessageBox.Show(string.Format("Display mode {0}x{1}-{2} is not available windowed", mode.Width, mode.Height, mode.Format));
-            }
-            if (!D3DObject.CheckDeviceType(0, DevType, Format.X8R8G8B8, Format.X8R8G8B8, false)) {
-                MessageBox.Show(string.Format("Display mode {0}x{1}-{2} is not available fullscreen", mode.Width, mode.Height, mode.Format));
-            }
-
+    public class Program {
+        public static void Main(string[] args) {
             
-            var caps = D3DObject.GetDeviceCaps(0, DevType);
-            CreateFlags devBehaviorFlags = 0;
-            if (caps.DeviceCaps.HasFlag(DeviceCaps.HWTransformAndLight)) {
-                devBehaviorFlags |= RequestedVertexProcessing;
-            } else {
-                devBehaviorFlags |= CreateFlags.SoftwareVertexProcessing;
-            }
-            if (caps.DeviceCaps.HasFlag(DeviceCaps.PureDevice) && devBehaviorFlags.HasFlag(CreateFlags.HardwareVertexProcessing)) {
-                devBehaviorFlags |= CreateFlags.PureDevice;
-            }
-
-            PP = new PresentParameters() {
-                BackBufferWidth = 0,
-                BackBufferHeight = 0,
-                BackBufferFormat = Format.Unknown,
-                BackBufferCount = 1,
-                Multisample = MultisampleType.None,
-                MultisampleQuality = 0,
-                SwapEffect = SwapEffect.Discard,
-                DeviceWindowHandle = MainWindow.Handle,
-                Windowed = true,
-                EnableAutoDepthStencil = true,
-                AutoDepthStencilFormat = Format.D24S8,
-                PresentFlags = PresentFlags.None,
-                FullScreenRefreshRateInHertz = 0,
-                PresentationInterval = PresentInterval.Immediate
-            };
-
-            GDevice = new Device(D3DObject, 0, DevType, MainWindow.Handle, devBehaviorFlags, PP );
-
         }
+    }
 
-        protected void Exit() {
-            _running = false;
-            Application.Exit();
+    public class App {
+        private Device _device;
+        private SlimDX.Direct3D9.Font _font;
+        private Form _mainWindow;
+        
+        public App() {
+            
         }
-
-        public virtual int Run() {
-            while (_running) {
-                
-                Application.DoEvents();
-                if (Paused) {
-                    Thread.Sleep(20);
-                    continue;
-                }
-                if (!IsDeviceLost()) {
-                    UpdateScene(0.0f);
-                    DrawScene();
-                }
-            }
-            return 0;
+        public void Init(int width, int height, bool windowed) {
+            
         }
-        public void EnableFullScreenMode(bool enable) {
-            if (enable) {
-                if (!PP.Windowed) return;
-
-                var width = Screen.PrimaryScreen.Bounds.Width;
-                var height = Screen.PrimaryScreen.Bounds.Height;
-
-                PP.BackBufferFormat = Format.X8R8G8B8;
-                PP.BackBufferWidth = width;
-                PP.BackBufferHeight = height;
-                PP.Windowed = false;
-
-                MainWindow.FormBorderStyle = FormBorderStyle.None;
-                MainWindow.SetDesktopLocation(0,0);
-
-
-            } else {
-                if (PP.Windowed) return;
-                MainWindow.ClientSize = new Size(800,600);
-                PP.BackBufferFormat = Format.Unknown;
-                PP.BackBufferWidth = 800;
-                PP.BackBufferHeight = 600;
-                PP.Windowed = true;
-
-                MainWindow.FormBorderStyle = FormBorderStyle.FixedSingle;
-                MainWindow.SetDesktopLocation(100, 100);
-            }
-            OnLostDevice();
-            GDevice.Reset(PP);
-            OnResetDevice();
+        public void Update(float dt) {
+            
         }
-        public bool IsDeviceLost() {
-            var hr = GDevice.TestCooperativeLevel();
-            if (hr == ResultCode.DeviceLost) {
-                Thread.Sleep(20);
-                return true;
-            }
-            if (hr == ResultCode.DriverInternalError) {
-                MessageBox.Show("Driver internal error, exiting...");
-                Exit();
-                return true;
-            }
-            if (hr == ResultCode.DeviceNotReset) {
-                OnLostDevice();
-                GDevice.Reset(PP);
-                OnResetDevice();
-                return false;
-            }
-            return false;
+        public void Render() {
+            
+        }
+        public void Cleanup() {
+            
+        }
+        public void Quit() {
+            
         }
 
         protected static void ReleaseCom(ComObject o) {
---
+++ Core/Point.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SlimDX;
+
+namespace Core {
+    public struct Point : IEquatable<Point> {
+        public bool Equals(Point other) {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals(object obj) {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is Point && Equals((Point) obj);
+        }
+
+        public override int GetHashCode() {
+            unchecked {
+                return (X*397) ^ Y;
+            }
+        }
+
+        public static bool operator ==(Point left, Point right) {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Point left, Point right) {
+            return !left.Equals(right);
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        
+
+        public Point(int x, int y) : this() {
+            X = x;
+            Y = y;
+        }
+
+        public static Point operator +( Point p1, Point p2) {
+            return new Point(p1.X + p2.X, p1.Y + p2.Y);
+        }
+        public static Point operator +(Point p1, int i) {
+            return new Point(p1.X + i, p1.Y + i);
+        }
+        public static Point operator -(Point p1, Point p2) {
+            return new Point(p1.X - p2.X, p1.Y - p2.Y);
+        }
+        public static Point operator -(Point p1, int i) {
+            return new Point(p1.X -i, p1.Y -i);
+        }
+
+        public static Point operator /(Point p, int rhs) {
+            return new Point(p.X/rhs, p.Y/rhs);
+        }
+        public static Point operator *(Point p, int rhs) {
+            return new Point(p.X * rhs, p.Y * rhs);
+        }
+
+        public float Distance(Point p2) {
+            return (float) Math.Sqrt((X-p2.X)*(X-p2.X) + (Y-p2.Y)*(Y-p2.Y));
+        }
+        public bool InRect(Rectangle r) {
+            return r.Contains(X, Y);
+        }
+
+    }
+
+    public class Debug {
+        private static volatile Debug _debug;
+        private static readonly object SyncRoot = new object();
+        public static Debug Logger{
+            get {
+                if (_debug == null) {
+                    lock (SyncRoot) {
+                        if (_debug == null) {
+                            _debug = new Debug();
+                        }
+                    }
+                }
+                return _debug;
+            }
+
+        }
+        private readonly StreamWriter _out;
+
+        private Debug() {
+            _out = new StreamWriter( new FileStream("out.txt", FileMode.OpenOrCreate));
+        }
+        ~Debug() {
+            if (_out != null) {
+                _out.Close();
+            }
+        }
+
+        public void Print(string fmt, params object[] args) {
+            _out.WriteLine(fmt, args);
+        }
+        public void Endl(int nr = 1) {
+            for (int i = 0; i < nr; i++) {
+                _out.WriteLine();
+            }
+        }
+        public void Write(Vector3 v) {
+            _out.Write("x: {0} y: {1} z:{2}", v.X, v.Y, v.Z);
+        }
+        public void Write(bool b) {
+            _out.Write(b? "True":"False");
+        }
+        public void Write(string s) {
+            _out.Write(s);
+        }
+        public void Write(float f) {
+            _out.Write(f);
+        }
+        public void Write(int i) {
+            _out.Write(i);
+        }
+
+    }
+}
+
